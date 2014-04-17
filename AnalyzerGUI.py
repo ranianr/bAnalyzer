2c312e5972e0194af21b582b3a22c8fb73fa2397
Rania <ranianr91@gmail.com>
2014-04-17 22:48:57 +0200

    get the environment ready for python development

---
+++ AnalyzerGUI.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append('UI')
+
+
+from PyQt4 import QtCore, QtGui
+from Analyzer_extended import Ui_MainWindow_Extended
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+
+    #MainUI
+    MainWindow = QtGui.QMainWindow()
+    ui_main = Ui_MainWindow_Extended()
+    ui_main.setupUi(MainWindow)
+    ui_main.InitializeUI()
+    
+    MainWindow.show()
+
+    sys.exit(app.exec_())
---
+++ bAnalyzer.sh
@@ -0,0 +1,10 @@
+UI_Path=UI
+
+clear
+
+echo -e "\e[1;31m   Generating MainUI"
+
+pyuic4 -o $UI_Path/Analyzer.py $UI_Path/Analyzer.ui
+
+echo -e "\e[1;37m\nStarting Braingizer-Analyzer\n\e[0m"
+python AnalyzerGUI.py
